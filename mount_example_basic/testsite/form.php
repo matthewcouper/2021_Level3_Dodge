e471920055b4f999ecc529f2865fc89a5b449dfd
Matthew Couper <matthewcouper@mmc.school.nz>
2021-06-08 12:52:07 +1200

    Planets moving down the game panel

---
+++ mount_example_basic/testsite/form.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+
+<body>
+<form method = "post" action = "insernet.php">
+Name: <input type="text" name="name" /> <br />
+Email: <input type="text" name="email" /> <br />
+Password: <input type="password" name="password" /> <br />
+
+<p>
+<input type ="submit" value = "register" />
+</p>
+</form>
+</body>
+</html>
